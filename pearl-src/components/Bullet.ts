54d1d46268b4524f18a1405ee9d7e32ec1b32372
Thomas Boyt <me@thomasboyt.com>
2018-06-18 16:51:45 -0400

    Destroy offscreen bullets

--- pearl-src/components/Bullet.ts
+++ pearl-src/components/Bullet.ts
@@ -1,8 +1,32 @@
-import { Component, Physical } from 'pearl';
+import { Component, Physical, PolygonCollider } from 'pearl';
 import NetworkingHost from './networking/NetworkingHost';
 import BulletExplosion from './BulletExplosion';
+import { TILE_SIZE, WORLD_SIZE_WIDTH, WORLD_SIZE_HEIGHT } from '../constants';
+import Game from './Game';
+
+const worldWidth = TILE_SIZE * WORLD_SIZE_WIDTH;
+const worldHeight = TILE_SIZE * WORLD_SIZE_HEIGHT;
 
 export default class Bullet extends Component<null> {
+  update(dt: number) {
+    // clean up off screen bullets
+    if (!this.pearl.obj.getComponent(Game).isHost) {
+      return;
+    }
+
+    const collider = this.getComponent(PolygonCollider);
+    const bounds = collider.getBounds();
+
+    if (
+      bounds.xMin < 0 ||
+      bounds.xMax > worldWidth ||
+      bounds.yMin < 0 ||
+      bounds.yMax > worldHeight
+    ) {
+      this.pearl.entities.destroy(this.gameObject);
+    }
+  }
+
   explode() {
     this.pearl.entities.destroy(this.gameObject);
 
